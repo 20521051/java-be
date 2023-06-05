48571701b9a6796206812f68c1740c5baeea56aa
Hoàng An <20521051@gm.uit.edu.vn>
2023-06-06 00:42:43 +0700

    config: mongo atlas and MVC

--- src/main/java/com/backend/store/Application.java
+++ src/main/java/com/backend/store/Application.java
@@ -2,16 +2,21 @@ package com.backend.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableMongoRepositories
+@Configuration
+@EnableWebMvc
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("\n[Swagger-ui]: http://localhost:8080/swagger-ui/index.html");
         System.out.println("[Swagger-api-docs]: http://localhost:8080/v3/api-docs");
-        System.out.println("[MongoDB]: http://localhost:27017/mydatabase");
+        System.out.println("[MongoDB]: http://localhost:27017/Future");
         System.out.println("[Server]: http://localhost:8080/");
     }
 }
